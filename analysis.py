b197e8da1c3fb367f6be84c7103777781eff64ca
ethansmadjaa <ethansmadja@icloud.com>
2024-12-26 03:47:29 +0100

    🎨 Add Plotly and Spark imports to analysis module

--- analysis.py
+++ analysis.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import plotly.graph_objects as go
+from pyspark.sql import SparkSession, DataFrame
 
 
 def analyze_data(spark_session, stock, days):
     st.title("Data Analysis")
     st.text("Module in development ! 🚀")
+    
